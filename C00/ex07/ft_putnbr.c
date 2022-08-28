69281f51150561ed8f30c926715a4a78fc664fb4
onurseen <50184870+onurseen@users.noreply.github.com>
2022-08-28 20:30:23 +0300

    Add files via upload

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: osen <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/27 18:56:39 by osen              #+#    #+#             */
-/*   Updated: 2022/08/27 20:08:35 by osen             ###   ########.fr       */
+/*   Created: 2022/08/28 18:08:21 by osen              #+#    #+#             */
+/*   Updated: 2022/08/28 19:19:40 by osen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,24 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_print_comb2(void)
+void	ft_putnbr(int nb)
 {
-	int	x;
-	int	y;
-
-	x = 0;
-	while (x < 99)
+	if (nb == -2147483648)
 	{
-		y = x + 1;
-		while (y <= 99)
+		ft_putnbr(nb / 10);
+		ft_putchar('8');
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else
+	{
+		if (nb > 9)
 		{
-			ft_putchar(48 + x / 10);
-			ft_putchar(48 + x % 10);
-			ft_putchar(' ');
-			ft_putchar(48 + y / 10);
-			ft_putchar(48 + y % 10);
-			if (x != 98 || y != 99)
-			{
-				ft_putchar(',');
-				ft_putchar(' ');
-			}
-			y++;
+			ft_putnbr(nb / 10);
 		}
-		x++;
+		ft_putchar(48 + nb % 10);
 	}
 }
