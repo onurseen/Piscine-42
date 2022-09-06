c52077d6b67124afbf31fa03ef6d616bfc0d6b6d
onurseen <50184870+onurseen@users.noreply.github.com>
2022-09-06 17:02:15 +0000

    Delete C01 directory

--- C01/ex05/ft_putstr_main.c
+++
@@ -1,8 +0,0 @@
-void	ft_putstr(char *str);
-
-int	main(void)
-{
-	char a[10] = "abcdesse";
-	ft_putstr(a);
-	return 0;
-}
--- C01/ex06/ft_strlen_main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int	ft_strlen(char *str);
-
-int	main(void)
-{
-	char	x[10] = "123456789";
-	printf("%d", ft_strlen(x));
-}
